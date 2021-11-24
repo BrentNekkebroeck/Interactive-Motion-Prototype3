8ee79137867aa864aa9e2d3b773fcb08011f2057
Brent Nekkebroeck <brent.nekkebroeck@student.ehb.be>
2021-11-24 15:16:10 +0100

    code(finished): finished code

---
+++ README.md
@@ -0,0 +1,11 @@
+Welcome to my Nexrender test.
+
+# Starting it up
+    - $ node app.js
+
+# Filling in properties
+    - On top of the app.js file
+
+# Note
+    - The longer the audio the longer the render. Always fixable with skipping frames on render. Good for testing!
+    - MAX 10 minute song.
--- app.js
+++ app.js
@@ -1,91 +1,120 @@
 const { render } = require('@nexrender/core')
+const { getAudioDurationInSeconds } = require('get-audio-duration');
 
 
-let job =  {
-    template: {
-        "src":"file:///d:/Interactive-Motion/Nexrender/Prototype/PrototypeAe.aep",
-        "composition": "Final"
-},
-"assets": [
-    {
-        "composition": "Final",
-        "src": "file:///d:/Interactive-Motion/Nexrender//Prototype/Assets/newplace.jpg",
-        "type": "image",
-        "layerIndex": "8"
-    },
-    {
-        "src": "file:///d:/Interactive-Motion/Nexrender//Prototype/Assets/testsong.mp3",
-        "type": "audio",
-        "layerName": "audio"
-    },
-    {   
-        "composition": "Banner",
-        "type": "data",
-        "layerIndex": "1",
-        "property": "Source Text",
-        "value": "Chime - Lifelong"
-    },
-    {   
-        "composition": "Banner",
-        "type": "data",
-        "layerIndex": "4",
-        "property": "Source Text",
-        "value": "Kiki Rama"
-    },
-    {
-        "composition": "Final",
-        "type": "data",
-        "layerIndex": "9",
-        "property": "Effects.Kleur1.Color",
-        "value": [1,0,0]
-    },
-    {
-        "composition": "Final",
-        "type": "data",
-        "layerIndex": "9",
-        "property": "Effects.Kleur2.Color",
-        "value": [1,0,0]
-    },
-    {
-        "composition": "Final",
-        "type": "data",
-        "layerIndex": "9",
-        "property": "Effects.Kleur3.Color",
-        "value": [1,0,0]
-    },
-    {
-        "composition": "Final",
-        "type": "data",
-        "layerIndex": "9",
-        "property": "Effects.Kleur4.Color",
-        "value": [1,0,0]
-    },
 
+function videoAutomatisation(){
+    // HERE IS THE INPUT OF PROPERTIES YOU WANT TO CHANGE
+        //Song
+        let theSong = "Assets/Songs/testsong.mp3";
+        //Background Image
+        let theBackgroundImage = "Assets/Backgrounds/newplace.jpg";
+        //Song Name       (note: the longer the song the longer the render)
+        let theSongName = "Riot - Overkill";
+        //Video Maker aka your name
+        let yourName = "Bertshj";
+        //Colors of the ball
+        let gradientColor1 = [1,0,0];
+        let gradientColor2 = [0,1,0];
+        let gradientColor3 = [1,1,0];
+        let gradientColor4 = [0,1,1];
+   
+    //getting the frames to set a automatic endtime depending on the songtime.
+    getAudioDurationInSeconds(theSong).then((duration) => {
+    let songTime = duration * 25;
+    let songTimeFrames = songTime.toString();
   
-
-],
-"actions":{
-    "postrender": [
+    //the json that gets pushed to the render
+    let job =  {
+        template: {
+            "src":"file:///d:/Interactive-Motion/Nexrender/Prototype/PrototypeAe.aep",
+            "composition": "Final",
+            "frameStart": "1",
+            "frameEnd": `${songTimeFrames}`
+    },
+    "assets": [
         {
-            "module": "@nexrender/action-encode",
-            "preset": "mp4",
-            "output": "encoded.mp4"
+            "composition": "Final",
+            "src": `file:///d:/Interactive-Motion/Nexrender//Prototype/${theBackgroundImage}`,
+            "type": "image",
+            "layerIndex": "6"
         },
         {
-            "module": "@nexrender/action-copy",
-            "input": "encoded.mp4",
-            "output": "d:/Interactive-Motion/Nexrender/Prototype/render.mp4"
-        }
-    ]
+            "src": `file:///d:/Interactive-Motion/Nexrender//Prototype/${theSong}`,
+            "type": "audio",
+            "layerName": "audio"
+        },
+        {   
+            "composition": "Banner",
+            "type": "data",
+            "layerIndex": "1",
+            "property": "Source Text",
+            "value": `${theSongName}`
+        },
+        {   
+            "composition": "Banner",
+            "type": "data",
+            "layerIndex": "4",
+            "property": "Source Text",
+            "value": `${yourName}`
+        },
+        {
+            "composition": "Final",
+            "type": "data",
+            "layerIndex": "7",
+            "property": "Effects.Kleur1.Color",
+            "value": gradientColor1
+        },
+        {
+            "composition": "Final",
+            "type": "data",
+            "layerIndex": "7",
+            "property": "Effects.Kleur2.Color",
+            "value": gradientColor2
+        },
+        {
+            "composition": "Final",
+            "type": "data",
+            "layerIndex": "7",
+            "property": "Effects.Kleur3.Color",
+            "value": gradientColor3
+        },
+        {
+            "composition": "Final",
+            "type": "data",
+            "layerIndex": "7",
+            "property": "Effects.Kleur4.Color",
+            "value": gradientColor4
+        },
+    
+      
+    
+    ],
+    "actions":{
+        "postrender": [
+            {
+                "module": "@nexrender/action-encode",
+                "preset": "mp4",
+                "output": "encoded.mp4"
+            },
+            {
+                "module": "@nexrender/action-copy",
+                "input": "encoded.mp4",
+                "output": "d:/Interactive-Motion/Nexrender/Prototype/render.mp4"
+            }
+        ]
+    }
+    }
+    
+    const main = async () => {
+        const result = await render(job, {
+            skipCleanup: true,
+        });
+    }
+    
+    main().catch(console.error);
+    
+});
 }
-}
 
-
-const main = async () => {
-    const result = await render(job, {
-        skipCleanup: true,
-    });
-}
-
-
-main().catch(console.error);
+videoAutomatisation();
